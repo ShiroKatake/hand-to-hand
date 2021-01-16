277346eb6805bb4d482212be1de0cc6e23df54f4
ShiroKatake <tgkhang262@gmail.com>
2021-01-16 16:23:16 +1100

    ADD: Script for handling hand spawn

---
+++ Assets/Scripts/Player/Hand.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HandSide
+{
+	Left,
+	Right
+}
+
+public class Hand : MonoBehaviour
+{
+	[SerializeField] private HandSide handSide;
+	[SerializeField] private Transform spawnPoint;
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void CheckHand()
+	{
+		if (handSide == HandSide.Left)
+		{
+
+		}
+		else if (handSide == HandSide.Right)
+		{
+
+		}
+	}
+}
