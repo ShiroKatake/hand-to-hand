844ce1e75cf140af571d97afbaa01578cfb339d0
ShiroKatake <tgkhang262@gmail.com>
2021-01-19 14:36:09 +1100

    UPDATE: Player can now change between idle and gun

--- Assets/Scripts/Player/PlayerShootingController.cs
+++ Assets/Scripts/Player/PlayerShootingController.cs
@@ -10,10 +10,18 @@ public class PlayerShootingController : PrivateInstanceSerializableSingleton<Pla
     //Non-Serialized Fields------------------------------------------------------------------------
 
     private bool canShoot = true;
-    private bool shootLeft;
+
+	private bool animationSet;
+
+	private bool leftHandAnimationIsSet;
+	private bool rightHandAnimationIsSet;
+
+	private bool shootLeft;
     private bool shootRight;
     private bool tab;
     private bool shift;
+
+	private Player player;
 
     //Public Properties------------------------------------------------------------------------------------------------------------------------------
 
@@ -24,16 +32,21 @@ public class PlayerShootingController : PrivateInstanceSerializableSingleton<Pla
     /// </summary>
     public bool CanShoot { get => canShoot; set => canShoot = value; }
 
-    //Core Recurring Methods-------------------------------------------------------------------------------------------------------------------------
+	private void Start()
+	{
+		player = Player.Instance;
+	}
 
-    /// <summary>
-    /// Update() is run every frame.
-    /// </summary>
-    private void Update()
+	//Core Recurring Methods-------------------------------------------------------------------------------------------------------------------------
+
+	/// <summary>
+	/// Update() is run every frame.
+	/// </summary>
+	private void Update()
     {
         GetInput();
-        CheckShooting();
-    }
+		CheckShooting();
+	}
 
     //Recurring Methods (Update())-------------------------------------------------------------------------------------------------------------------
 
@@ -46,40 +59,62 @@ public class PlayerShootingController : PrivateInstanceSerializableSingleton<Pla
         tab = Input.GetButton("Tab");
         shootLeft = !shift && !tab && Input.GetButton("Left Hand");
         shootRight = !shift && !tab && Input.GetButton("Right Hand");
-        Debug.Log($"PlayerShootingController.GetInput(), shift: {shift}, tab: {tab}, shootLeft: {shootLeft}, leftHandWeapon.Stats.TriggerDown: {Player.Instance.HandController.LeftHandWeapon?.CurrentStats.TriggerDown} shootRight: {shootRight}, rightHandWeapon.Stats.TriggerDown: {Player.Instance.HandController.RightHandWeapon?.CurrentStats.TriggerDown}");
+        Debug.Log($"PlayerShootingController.GetInput(), shift: {shift}, tab: {tab}, shootLeft: {shootLeft}, leftHandWeapon.Stats.TriggerDown: {player.HandController.LeftHandWeapon?.CurrentStats.TriggerDown} shootRight: {shootRight}, rightHandWeapon.Stats.TriggerDown: {player.HandController.RightHandWeapon?.CurrentStats.TriggerDown}");
     }
 
     /// <summary>
     /// Checks if the player wants to and can shoot based on their input, and tells the appropriate weapon(s) to shoot if they do.
     /// </summary>
     private void CheckShooting()
-    {        
+    {
         if (shootLeft)
-        {
-            if (ReadyToShoot(Player.Instance.HandController.LeftHandWeapon, shootLeft)) Player.Instance.HandController.LeftHandWeapon.Shoot();
+		{
+			leftHandAnimationIsSet = SetAnimationToPistol(player.LeftHandAnimator, leftHandAnimationIsSet);
+			if (ReadyToShoot(player.HandController.LeftHandWeapon, shootLeft)) player.HandController.LeftHandWeapon.Shoot();
         }
         else
         {
-            if (Player.Instance.HandController.LeftHandWeapon.CurrentStats != null) Player.Instance.HandController.LeftHandWeapon.CurrentStats.TriggerDown = false;
-        }
+            if (player.HandController.LeftHandWeapon.CurrentStats != null) player.HandController.LeftHandWeapon.CurrentStats.TriggerDown = false;
+		}
 
         if (shootRight)
         {
-            if (ReadyToShoot(Player.Instance.HandController.RightHandWeapon, shootRight)) Player.Instance.HandController.RightHandWeapon.Shoot();
+			rightHandAnimationIsSet = SetAnimationToPistol(player.RightHandAnimator, rightHandAnimationIsSet);
+			if (ReadyToShoot(player.HandController.RightHandWeapon, shootRight)) player.HandController.RightHandWeapon.Shoot();
         }
         else
         {
-            if (Player.Instance.HandController.RightHandWeapon.CurrentStats != null) Player.Instance.HandController.RightHandWeapon.CurrentStats.TriggerDown = false;
+			if (player.HandController.RightHandWeapon.CurrentStats != null) player.HandController.RightHandWeapon.CurrentStats.TriggerDown = false;
         }
     }
 
-    /// <summary>
-    /// Checks if the player is able to shoot and their weapon can shoot.
-    /// </summary>
-    /// <param name="weapon">The weapon that the player would shoot with.</param>
-    /// <param name="triggerDown">Is the player holding down the weapon's trigger?</param>
-    /// <returns>Is the player able to shoot and can their weapon shoot right now?</returns>
-    private bool ReadyToShoot(Weapon weapon, bool triggerDown)
+	private bool SetAnimationToPistol(Animator animator, bool handAnimation)
+	{
+		if (animator.GetCurrentAnimatorStateInfo(0).IsName("Finger Pistol"))
+			return handAnimation;
+		
+		if (!handAnimation)
+		{
+			animator.SetTrigger("Pistol");
+			handAnimation = true;
+		}
+
+		return handAnimation;
+	}
+
+	public void ResetAnimationToIdle(Animator animator, bool handAnimation)
+	{
+		animator.SetTrigger("Idle");
+		handAnimation = false;
+	}
+
+	/// <summary>
+	/// Checks if the player is able to shoot and their weapon can shoot.
+	/// </summary>
+	/// <param name="weapon">The weapon that the player would shoot with.</param>
+	/// <param name="triggerDown">Is the player holding down the weapon's trigger?</param>
+	/// <returns>Is the player able to shoot and can their weapon shoot right now?</returns>
+	private bool ReadyToShoot(Weapon weapon, bool triggerDown)
     {
         Debug.Log($"PlayerShootingController.ReadyToShoot()");
         return canShoot && weapon != null && weapon.CurrentStats != null && weapon.ReadyToShoot(triggerDown);
