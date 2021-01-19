b1c3bd3be85b0979819a79cef9521a4a99f434ee
ShiroKatake <tgkhang262@gmail.com>
2021-01-19 15:57:38 +1100

    UPDATE: Realigned hands to shoot at the same point, added FX to explosion script

--- Assets/Scripts/Equipment/Grenade.cs
+++ Assets/Scripts/Equipment/Grenade.cs
@@ -8,93 +8,96 @@ public class Grenade : MonoBehaviour
 	//Private Fields---------------------------------------------------------------------------------------------------------------------------------  
 
 	//Serialized Fields----------------------------------------------------------------------------    
-    
+
 	[SerializeField] private float explosionRadius;
 	[SerializeField] private float explosionForce;
 	[SerializeField] private float cookTime = 3f;
+	[SerializeField] private GameObject explosionFX;
+	[SerializeField] private GameObject handRenderer;
 
-    //Non-Serialized Fields------------------------------------------------------------------------                                                    
+	//Non-Serialized Fields------------------------------------------------------------------------                                                    
 
-    private AudioSource audioSource;
-    private bool exploding = false;
+	private AudioSource audioSource;
+	private bool exploding = false;
 	private float timePassed;
 
-    //Public Properties------------------------------------------------------------------------------------------------------------------------------
+	//Public Properties------------------------------------------------------------------------------------------------------------------------------
 
-    //Basic Public Properties----------------------------------------------------------------------                                                                                                                          
+	//Basic Public Properties----------------------------------------------------------------------                                                                                                                          
 
-    /// <summary>
-    /// Is the grenade about to explode?
-    /// </summary>
-    public bool Exploding { get => exploding; set => exploding = value; }
+	/// <summary>
+	/// Is the grenade about to explode?
+	/// </summary>
+	public bool Exploding { get => exploding; set => exploding = value; }
 
-    //Complex Public Properties--------------------------------------------------------------------                                                    
+	//Complex Public Properties--------------------------------------------------------------------                                                    
 
 
 
-    //Initialization Methods-------------------------------------------------------------------------------------------------------------------------
+	//Initialization Methods-------------------------------------------------------------------------------------------------------------------------
 
-    /// <summary>
-    /// Awake() is run when the script instance is being loaded, regardless of whether or not the script is enabled. 
-    /// Awake() runs before Start().
-    /// </summary>
-    private void Awake()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
+	/// <summary>
+	/// Awake() is run when the script instance is being loaded, regardless of whether or not the script is enabled. 
+	/// Awake() runs before Start().
+	/// </summary>
+	private void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
 
-    /// <summary>
-    /// Start() is run on the frame when a script is enabled just before any of the Update methods are called for the first time. 
-    /// Start() runs after Awake().
-    /// </summary>
-    private void Start()
-    {
+	/// <summary>
+	/// Start() is run on the frame when a script is enabled just before any of the Update methods are called for the first time. 
+	/// Start() runs after Awake().
+	/// </summary>
+	private void Start()
+	{
+		handRenderer.SetActive(true);
+		explosionFX?.SetActive(false);
+	}
 
-    }
+	//Core Recurring Methods-------------------------------------------------------------------------------------------------------------------------
 
-    //Core Recurring Methods-------------------------------------------------------------------------------------------------------------------------
-
-    /// <summary>
-    /// Update() is run every frame.
-    /// </summary>
-    private void Update()
-    {
+	/// <summary>
+	/// Update() is run every frame.
+	/// </summary>
+	private void Update()
+	{
 		if (exploding)
 		{
 			if (timePassed >= cookTime)
 				Explode();
 			timePassed += Time.deltaTime;
 		}
-    }
+	}
 
-    /// <summary>
-    /// FixedUpdate() is run at a fixed interval independant of framerate.
-    /// </summary>
-    private void FixedUpdate()
-    {
+	/// <summary>
+	/// FixedUpdate() is run at a fixed interval independant of framerate.
+	/// </summary>
+	private void FixedUpdate()
+	{
 
-    }
+	}
 
-    //Recurring Methods (Update())------------------------------------------------------------------------------------------------------------------  
-
-
-
-    //Recurring Methods (FixedUpdate())--------------------------------------------------------------------------------------------------------------
+	//Recurring Methods (Update())------------------------------------------------------------------------------------------------------------------  
 
 
 
-    //Recurring Methods (Other)----------------------------------------------------------------------------------------------------------------------
+	//Recurring Methods (FixedUpdate())--------------------------------------------------------------------------------------------------------------
 
 
 
-    //Triggered Methods------------------------------------------------------------------------------------------------------------------------------
+	//Recurring Methods (Other)----------------------------------------------------------------------------------------------------------------------
+
+
+
+	//Triggered Methods------------------------------------------------------------------------------------------------------------------------------
 
 	/// <summary>
 	/// Explode the grenade.
 	/// </summary>
 	private void Explode()
 	{
-        Debug.Log($"{this}.Grenade.Explode()");
+		Debug.Log($"{this}.Grenade.Explode()");
 		Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
 
 		foreach (Collider collider in colliders)
@@ -111,7 +114,9 @@ public class Grenade : MonoBehaviour
 			}
 		}
 
-        audioSource.Play();
+		handRenderer.SetActive(false);
+		explosionFX?.SetActive(true);
+		audioSource.Play();
 
 		//Return hand back to pool
 		exploding = false;
