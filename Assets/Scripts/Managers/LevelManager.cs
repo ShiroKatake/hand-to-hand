f68e2edaa8b60be6ace172c33f51c8686cdc6dd1
ShiroKatake <tgkhang262@gmail.com>
2021-01-21 19:26:40 +1100

    ADD: Level manager

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum ELevels
+{
+	MainMenu,
+	Gameplay
+}
+
+public class LevelManager : MonoBehaviour
+{
+	public static LevelManager Instance { get; set; }
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void LoadScene(ELevels level)
+	{
+		SceneManager.LoadScene((int)level);
+	}
+}
