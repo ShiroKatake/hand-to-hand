d571438758f37981e2ebe1f95b1195dd71f0550b
Sam Huffer <samhuffer01@gmail.com>
2021-01-21 12:27:00 +1100

    Hands now detonate once being shot, once the timer expires of course. Their durability can be updated later if necessary.

---
+++ Assets/Scripts/Health + Death Controllers/HandDestructionController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A health classa and destruction controller for hands.
+/// </summary>
+public class HandDestructionController : Health
+{
+    //Private Fields---------------------------------------------------------------------------------------------------------------------------------
+
+    //Non-Serialized Fields------------------------------------------------------------------------
+
+    private Grenade grenade;
+
+    //Initialization Methods-------------------------------------------------------------------------------------------------------------------------
+
+    private void Awake()
+    {
+        grenade = GetComponent<Grenade>();
+    }
+
+    //Triggered Methods------------------------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// Handles the destruction of hands.
+    /// </summary>
+    protected override void Die()
+    {
+        grenade.PullPin();
+        Destroy(this);
+    }
+}
