08f03dc9d706925da426827648b140bd5da4de3a
ShiroKatake <tgkhang262@gmail.com>
2021-01-21 21:18:54 +1100

    ADD: Game over screen

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+	[SerializeField] GameObject gameOverScreenContainer;
+	[SerializeField] GameObject winText;
+	[SerializeField] GameObject loseText;
+
+    public void SetGameOver(bool victory)
+	{
+		if (victory)
+		{
+			loseText.SetActive(false);
+			winText.SetActive(true);
+			gameOverScreenContainer.SetActive(true);
+		}
+		else
+		{
+			loseText.SetActive(true);
+			winText.SetActive(false);
+			gameOverScreenContainer.SetActive(true);
+		}
+
+		Time.timeScale = 0;
+	}
+}
